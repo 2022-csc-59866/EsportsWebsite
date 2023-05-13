d46baf75a535d92425e9764cc33355e10d2b1f17
Gazi Shahi <gazishahi@gmail.com>
2023-05-12 23:22:23 -0400

    Local Strategy Functionally Complete

--- test/src/Components/Login.jsx
+++ test/src/Components/Login.jsx
@@ -1,16 +1,11 @@
-import React, {useState, useEffect} from "react";
-import { useAuth0 } from "@auth0/auth0-react";
+import React, { useState } from "react";
 import { Link } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css'
 import httpclient from "../httpclient";
 import './Login.css';
 
 export default function Login() {
-    const { data, setData} = useState([{}])
-    const { loginWithRedirect, isAuthenticated, isLoading } = useAuth0();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [authenticated, setAuthenticated] = useState(false)
 
     const logInUser = async () => {
         console.log(email, password);
@@ -21,8 +16,6 @@ export default function Login() {
             password,
           });
           window.location.href = "/";
-          setAuthenticated(true);
-          console.log(authenticated)
         } catch (error) {
           if (error.response.status === 401) {
             alert("Invalid credentials");
@@ -30,16 +23,7 @@ export default function Login() {
         }
       };
 
-    if (isLoading) {
-        return <div>Loading ...</div>;
-    }
-
     return (
-        // !isAuthenticated && (
-        //     <div>
-        //         <button className="log-btn" onClick={() => loginWithRedirect()}>Sign In</button> 
-        //     </div>
-        // )
         <div className="login-form">
 
                 <h1>Sign In</h1>
@@ -62,23 +46,6 @@ export default function Login() {
                         </Link>
                     </div>
                 </form>
-            
-
-            {/* <h2>Sign In</h2>
-            <input type="email" placeholder="Email" value={email} onChange={(event) => setEmail(event.target.value)}/>
-            <input type="password" placeholder="Password" value={password} onChange={(event) => setPassword(event.target.value)}/>
-            <div> 
-                <a href="/login"><button>Sign In</button></a>
-                <a href="/register"><button>Register</button></a>
-            </div> */}
-            
-                    {/* {(typeof data.members === 'undefined') ? (
-                        <p>Loading...</p>
-                    ): (
-                        data.members.map((members, i) => (
-                            <p key={i}>{members}</p>
-                        ))
-                    )} */}
         </div>
         
     )
--- test/src/Components/Register.jsx
+++ test/src/Components/Register.jsx
@@ -1,16 +1,11 @@
-import React, {useState, useEffect} from "react";
-import { useAuth0 } from "@auth0/auth0-react";
+import React, { useState } from "react";
 import { Link } from 'react-router-dom';
-import './Login.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
 import httpclient from "../httpclient";
+import './Login.css';
 
 export default function Login() {
-    const { data, setData} = useState([{}])
-    const { loginWithRedirect, isAuthenticated, isLoading } = useAuth0();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [authenticated, setAuthenticated] = useState(false)
 
     const registerUser = async () => {
         console.log(email, password);
@@ -21,8 +16,6 @@ export default function Login() {
             password,
           });
           window.location.href = "/";
-          setAuthenticated(true);
-          console.log(authenticated)
         } catch (error) {
           if (error.response.status === 401) {
             alert("Invalid credentials");
@@ -30,53 +23,25 @@ export default function Login() {
         }
       };
 
-    if (isLoading) {
-        return <div>Loading ...</div>;
-    }
-
     return (
-        // !isAuthenticated && (
-        //     <div>
-        //         <button className="log-btn" onClick={() => loginWithRedirect()}>Sign In</button> 
-        //     </div>
-        // )
         <div className="login-form">
-
-                <h1>Create Account</h1>
-                <form>
-                    <div>
-                        <label>Email </label>
-                        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email Address" id=""></input>
-                    </div>
-                    <div>
-                        <label>Password </label>
-                        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" id=""></input>
-                    </div>
-                    <button className="login-btn" type="button" onClick={() => registerUser()}>
-                        Submit
-                    </button>
-                    <div>
-                    <Link to="/auth">Return to sign in</Link>
-                    </div>
-                </form>
-            
-
-            {/* <h2>Sign In</h2>
-            <input type="email" placeholder="Email" value={email} onChange={(event) => setEmail(event.target.value)}/>
-            <input type="password" placeholder="Password" value={password} onChange={(event) => setPassword(event.target.value)}/>
-            <div> 
-                <a href="/login"><button>Sign In</button></a>
-                <a href="/register"><button>Register</button></a>
-            </div> */}
-            
-                    {/* {(typeof data.members === 'undefined') ? (
-                        <p>Loading...</p>
-                    ): (
-                        data.members.map((members, i) => (
-                            <p key={i}>{members}</p>
-                        ))
-                    )} */}
+            <h1>Create Account</h1>
+            <form>
+                <div>
+                    <label>Email </label>
+                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email Address" id=""></input>
+                </div>
+                <div>
+                    <label>Password </label>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" id=""></input>
+                </div>
+                <button className="login-btn" type="button" onClick={() => registerUser()}>
+                    Submit
+                </button>
+                <div>
+                <Link to="/auth">Return to sign in</Link>
+                </div>
+            </form>
         </div>
-        
     )
 }
