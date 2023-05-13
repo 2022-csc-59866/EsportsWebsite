126d82846073df0ccd28e3b78fb3f5d0fc53cb2c
Melson Heo <melsonheo646@gmail.com>
2023-05-12 22:59:47 -0400

    commit message

--- test/src/Components/ContactUs.jsx
+++ test/src/Components/ContactUs.jsx
@@ -1,8 +1,37 @@
-export default function ContactUs() {
+import React, { useState } from 'react';
+
+function MessageBox() {
+  const [message, setMessage] = useState('');
+
+  const handleMessageChange = (event) => {
+    setMessage(event.target.value);
+  };
+
   return (
-      <div>
-      <h1>Contact Us</h1>
-      <p>Have any questions? Feel free to contact us!</p>
+    <div className="message-box">
+      <textarea
+        style={{ height: "200px", width: "50%" }}
+        placeholder="Type your message here"
+        value={message}
+        onChange={handleMessageChange}
+      />
+      <br></br>
+      <button style={{color: "greenyellow"}}>Send</button>
+      <br></br>
     </div>
   );
-  }
+}
+
+export default function ContactUs() {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>Have any questions? Feel free to contact us!</p>
+      <p>Owner: Melson Heo and Gazi Shahi</p>
+      <MessageBox />
+      <br></br>
+      <p>For any business inquiries, feel free to email at esportsfrenzy@gmail.com</p>
+      <p>For any other inquiries, set an appointment with us by calling 646-572-3452</p>
+    </div>
+  );
+}
--- test/src/Components/League.jsx
+++ test/src/Components/League.jsx
@@ -35,7 +35,7 @@ export default function League() {
   const [isHovered3, setIsHovered3] = useState(false);
   const [isHovered4, setIsHovered4] = useState(false);
   const apiCalledRef = useRef(false); //To prevent repeating API calls
-  const API_KEY = "RGAPI-4dd164ea-391a-4d1a-954a-b827fb80e4c0"; //API Key
+  const API_KEY = "RGAPI-34f1e571-1bb9-49bd-b901-48ae0a573601"; //API Key
 
   useEffect(() => {
     const fetchData = async () => {
