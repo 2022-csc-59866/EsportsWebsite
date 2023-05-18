7d7cbda4b4c96527842eb6a93006e9e261b5446c
Gazi Shahi <gazishahi@gmail.com>
2023-05-17 23:02:12 -0400

    Updating permissions to allow cookies/sessions

--- flask-server/config.py
+++ flask-server/config.py
@@ -13,3 +13,5 @@ class ApplicationConfig:
     SESSION_PERMANENT = False
     SESSION_USE_SIGNER = True
     SESSION_REDIS = redis.from_url("redis://red-chio9ae4dad01annfng0:6379")
+    SESSION_COOKIE_SECURE = True
+    SESSION_COOKIE_HTTPONLY = True
