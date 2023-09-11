9582ed4f60a7a2f164d09b78bc379c3b1abafc94
Gazi Shahi <gazishahi@gmail.com>
2023-09-10 21:15:44 -0400

    Fix API Keys

--- flask-server/config.py
+++ flask-server/config.py
@@ -7,11 +7,11 @@ class ApplicationConfig:
     SECRET_KEY = os.environ["SECRET_KEY"]
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
-    SQLALCHEMY_DATABASE_URI = r"postgresql://csbctgwl:UO4cAHExA9WgIS-BjhN1pFRoZ8QLsOfZ@drona.db.elephantsql.com/csbctgwl"
+    SQLALCHEMY_DATABASE_URI = ["DATABASE"]
 
     SESSION_TYPE = "redis"
     SESSION_PERMANENT = False
     SESSION_USE_SIGNER = True
-    SESSION_REDIS = redis.from_url("rediss://red-chio9ae4dad01annfng0:At2i6kqMwQjEq782u8dY3NvBHWZyVrX0@ohio-redis.render.com:6379")
+    SESSION_REDIS = redis.from_url("REDIS_URI")
     SESSION_COOKIE_SECURE = True
     SESSION_COOKIE_HTTPONLY = True
